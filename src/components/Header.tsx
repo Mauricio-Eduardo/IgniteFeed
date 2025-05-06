e9e3d47ba598a9b786b74f7689cef38dc84fa07c
Mauricio Eduardo <90938375+Mauricio-Eduardo@users.noreply.github.com>
2025-05-06 09:40:25 -0300

    update logo src

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export function Header() {
     return (
     <header className={styles.header}>
-        <img src="../assets/ignite simbol.svg" alt='Logo do Ignite Feed'/>
+        <img src="igniteLogo.svg" alt='Logo do Ignite Feed'/>
         <strong>Ignite Feed</strong>
     </header>
     )
